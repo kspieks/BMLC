5fa9974166c6f467d4914666f9359d8812a696fc
Kevin Spiekermann <kspieker@mit.edu>
2024-01-05 17:04:28 -0500

    Add file to train the baseline models

---
+++ train_baseline_models.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+Baseline ML models for cheminformatics
+"""
+from baseline_models.main import BaselineML
+from utils.parsing import parse_command_line_arguments
+from utils.utils import create_logger
+
+def main():
+    """
+    The main executable function.
+    """
+
+    args, config_dict = parse_command_line_arguments()
+
+    logger = create_logger(args.log_name, args.log_dir)
+    logger.info('Using arguments...')
+    for arg in vars(args):
+        logger.info(f'{arg}: {getattr(args, arg)}')
+    logger.info('')
+
+    baseline_configs = config_dict['baseline_configs']
+    baseline_configs['logger'] = logger
+    baseline_ML = BaselineML(**baseline_configs)
+    baseline_ML.execute()
+
+
+if __name__ == '__main__':
+    main()
