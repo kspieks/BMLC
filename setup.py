ba1611a58e370218d1ff142b4e40cd89f69a3218
Kevin Spiekermann <kspieker@mit.edu>
2024-01-05 16:53:28 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+
+from setuptools import find_packages, setup
+
+__version__ = None
+
+
+# utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="BMLC (Baseline Machine Learning for Cheminformatics)",
+    version=__version__,
+    author="Kevin Spiekermann",
+    description="This codebase trains several baseline ML models from sklearn using various fingerprint representations from RDKit.",
+    url="https://github.com/kspieks/BMLC",
+    packages=find_packages(),
+    long_description=read('README.md'),
+)
