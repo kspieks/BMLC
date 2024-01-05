f0e0352b774aabf7271b88c23b5de66327cfc55d
Kevin Spiekermann <kspieker@mit.edu>
2024-01-05 17:00:59 -0500

    Add main function to train the models and tune hyperparameters

---
+++ baseline_models/training.py
@@ -0,0 +1,160 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+
+from .models import _MODELS
+
+
+def calc_regression_metrics(y_true, y_pred):
+    """Calculate performance metrics for the regression model"""
+    MAE = mean_absolute_error(y_true, y_pred)
+    RMSE = mean_squared_error(y_true, y_pred, squared=False)
+    R2 = r2_score(y_true, y_pred)
+    kendalltau = stats.kendalltau(y_true, y_pred)
+
+    return (MAE, RMSE, R2, kendalltau)
+
+
+def naive_baseline(y, splits, logger):
+    """
+    Naively predicts the mean value from the training set for all test molecules.
+    """
+    dfs_summary_tmp = [] 
+
+    for i, (train_indices, val_indices, test_indices) in enumerate(splits):
+        y_train = y[train_indices]
+        y_val = y[val_indices]
+        y_test = y[test_indices]
+
+        logger.info(f'Mean training value: {y_train.mean():.3f} kcal/mol')
+        logger.info(f'Mean validation value: {y_val.mean():.3f} kcal/mol')
+        logger.info(f'Mean testing value: {y_test.mean():.3f} kcal/mol\n')
+
+        # baseline model uses mean of the training set
+        y_pred_train = [y_train.mean()] * len(y_train)
+        y_pred_val = [y_train.mean()] * len(y_val)
+        y_pred_test = [y_train.mean()] * len(y_test)
+
+        (MAE, RMSE, R2, kendalltau) = calc_regression_metrics(y_train, y_pred_train)
+        dfs_summary_tmp.append(['train', i, MAE, RMSE, R2, kendalltau.statistic, kendalltau.pvalue])
+
+        (MAE, RMSE, R2, kendalltau) = calc_regression_metrics(y_val, y_pred_val)
+        dfs_summary_tmp.append(['val', i, MAE, RMSE, R2, kendalltau.statistic, kendalltau.pvalue])
+
+        (MAE, RMSE, R2, kendalltau) = calc_regression_metrics(y_test, y_pred_test)
+        dfs_summary_tmp.append(['test', i, MAE, RMSE, R2, kendalltau.statistic, kendalltau.pvalue])
+
+    cols = ['set', 'split', 
+            'MAE', 'RMSE', 'R2', 
+            'kendall_tau_statistic', 'kendal_tau_pvalue',
+            ]
+    df_summary = pd.DataFrame(dfs_summary_tmp, columns=cols)
+
+    return df_summary
+
+
+# Explore hyperparameters
+def train_model(model,
+                X,
+                y,
+                splits,
+                ):
+
+    dfs_pred_tmp = []
+    dfs_summary_tmp = []
+    models = []
+    scalers = []
+
+    for i, (train_indices, val_indices, test_indices) in enumerate(splits):
+        X_train = X[train_indices]
+        X_val = X[val_indices]
+        X_test = X[test_indices]
+
+        # reshape size to be N x 1
+        y_train = y[train_indices].reshape(-1, 1)
+        y_val = y[val_indices].reshape(-1, 1)
+        y_test = y[test_indices].reshape(-1, 1)
+
+        scaler = StandardScaler()
+        scaler.fit(y_train)
+        scalers.append(scaler)
+
+        y_train_scaled = scaler.transform(y_train)  # N x 1
+
+        # fit the model to the training data
+        model.fit(X_train, y_train_scaled.flatten())
+        models.append(model)
+
+        # get predictions. reshape size to be N x 1
+        y_pred_train = scaler.inverse_transform(model.predict(X_train).reshape(-1, 1))
+        y_pred_val = scaler.inverse_transform(model.predict(X_val).reshape(-1, 1))
+        y_pred_test = scaler.inverse_transform(model.predict(X_test).reshape(-1, 1))
+
+        data_dict = {'y_test': y_test.flatten(),
+                     'y_pred_test': y_pred_test.flatten(),
+                     'split': [i] * len(y_test),
+                     }
+        dfs_pred_tmp.append(pd.DataFrame(data_dict))
+
+        # store performance metrics
+        # training set
+        (MAE, RMSE, R2, kendalltau) = calc_regression_metrics(y_train, y_pred_train)
+        dfs_summary_tmp.append(['train', i, MAE, RMSE, R2, kendalltau.statistic, kendalltau.pvalue])
+
+        # validation set
+        (MAE, RMSE, R2, kendalltau) = calc_regression_metrics(y_val, y_pred_val)
+        dfs_summary_tmp.append(['val', i, MAE, RMSE, R2, kendalltau.statistic, kendalltau.pvalue])
+
+        # testing set
+        (MAE, RMSE, R2, kendalltau) = calc_regression_metrics(y_test, y_pred_test)
+        dfs_summary_tmp.append(['test', i, MAE, RMSE, R2, kendalltau.statistic, kendalltau.pvalue])
+
+    cols = ['set', 'split', 
+            'MAE', 'RMSE', 'R2', 
+            'kendall_tau_statistic', 'kendal_tau_pvalue',
+            ]
+    df_summary = pd.DataFrame(dfs_summary_tmp, columns=cols)
+
+    df_predictions = pd.concat(dfs_pred_tmp)
+
+    return df_summary, df_predictions, models, scalers
+
+
+# todo: could maybe use the `class_weight` parameter if our dataset is unbalanced
+class Objective:
+    def __init__(self,
+                 X,
+                 y,
+                 splits,
+                 model_type,
+                 _MODELS=_MODELS,
+                 random_state=42,
+                 ):
+        self.X = X
+        self.y = y
+        self.splits = splits
+        self.model_type = model_type
+
+        self._MODELS = _MODELS
+
+        self.random_state = random_state
+
+    def __call__(self, trial):
+        regressor_obj = self._MODELS[self.model_type](trial, random_state=self.random_state)
+
+        trial.set_user_attr(key="best_model", value=regressor_obj)
+
+        df_summary, _, _, _ = train_model(regressor_obj,
+                                          X=self.X,
+                                          y=self.y,
+                                          splits=self.splits,
+                                          )
+
+        return df_summary.query("set == 'val'")['RMSE'].mean()
+
+# add callback to easily recall the best model parameters
+def callback(study, trial):
+    if study.best_trial.number == trial.number:
+        study.set_user_attr(key="best_model", value=trial.user_attrs["best_model"])
