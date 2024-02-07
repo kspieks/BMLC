80b0d8a671e5cca30a13e6acde119b05bca1fc68
Kevin Spiekermann <kspieker@mit.edu>
2024-02-07 08:48:33 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -7,6 +7,7 @@ Script to make predictions using a pretrained model (or ensemble of trained mode
 
 import os
 import pickle as pkl
+from pprint import pprint
 
 import numpy as np
 import pandas as pd
@@ -14,7 +15,7 @@ from pandarallel import pandarallel
 
 from baseline_models.featurizers import _FP_FEATURIZERS, get_rxn_fp
 from baseline_models.utils.parsing import parse_prediction_command_line_arguments
-from baseline_models.utils.utils import create_logger
+from baseline_models.utils.utils import create_logger, read_yaml_file
 
 
 def main():
@@ -30,22 +31,52 @@ def main():
         logger.info(f'{arg}: {getattr(args, arg)}')
     logger.info('')
 
+    # replace filepath with dictionary of featurizer settings
+    featurizer_settings = read_yaml_file(args.featurizer_yaml_path)
+    logger.info('Featurizer settings:')
+    logger.info(pprint(featurizer_settings))
+    logger.info('')
+
     # read in data
     df = pd.read_csv(args.data_path)
     logger.info(f'{args.data_path} has {len(df):,} rows')
 
     # generate features in parallel
     pandarallel.initialize(nb_workers=args.n_cpus_featurize, progress_bar=True)
-    featurizer = args.featurizer
-    # reaction mode
-    if args.rxn_mode:
-        df[featurizer] = df.smiles.parallel_apply(get_rxn_fp, featurizer=_FP_FEATURIZERS[featurizer])
+    fp_arrays = []
+    for featurizer, parameter_dict in featurizer_settings.items():
+        params = parameter_dict['parameters']
+        logger.info(f'Calculating {featurizer} features...')
 
-    # molecule mode
-    else:
-        df[featurizer] = df.smiles.parallel_apply(_FP_FEATURIZERS[featurizer])
-    
-    X = np.stack(df[featurizer].values)
+        featurizers = featurizer.split('+')
+        if len(featurizers) != len(parameter_dict['parameters']):
+            msg = "Dimension mismatch!\n"
+            msg += f"There are {len(featurizers)} featurizers specified and "
+            msg += f"{len(parameter_dict['parameters'])} parameter dictionaries specified in the input json file.\n"
+            msg += "These values should be identical."
+            raise ValueError(msg)
+
+        fp_arrays = []
+        for f, params in zip(featurizers, parameter_dict['parameters']):
+            if len(featurizers) > 1:
+                # if there are only one set of features to calculate, don't print the same line twice
+                logger.info(f'Calculating {f} features...')
+            logger.info(f'Specified settings include\n{params}')
+
+            # reaction mode
+            if args.rxn_mode:
+                params['featurizer'] = _FP_FEATURIZERS[f]
+                fps = df[args.smiles_column].parallel_apply(get_rxn_fp, **params)
+
+            # molecule mode
+            else:
+                fps = df[args.smiles_column].parallel_apply(_FP_FEATURIZERS[f], **params)
+        
+            fp_array = np.stack(fps.values)
+            logger.info(f'Fingerprint array has shape {fp_array.shape}\n')
+            fp_arrays.append(fp_array)
+
+    X = np.hstack(fp_arrays)
     logger.info(f'X.shape: {X.shape}')
 
     # load models and scalers
@@ -73,7 +104,7 @@ def main():
         df_preds = pd.DataFrame(y_preds, columns=['pred'])
     
     # add smiles
-    df_preds.insert(0, 'smiles', df.smiles.values)
+    df_preds.insert(0, 'smiles', df[args.smiles_column].values)
     
     # save predictions
     df_preds.to_csv(os.path.join(args.save_dir, args.pred_name), index=False)
